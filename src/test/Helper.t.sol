96cfc98cf5f182662afefeea1e2456739bd7b8c7
08xmt <markus.08xmt@gmail.com>
2023-02-13 22:26:34 +0100

    Add secondary test

--- src/test/Helper.t.sol
+++ src/test/Helper.t.sol
@@ -17,12 +17,13 @@ import "./mocks/WETH9.sol";
 import "./mocks/BorrowContract.sol"; 
 import {EthFeed} from "./mocks/EthFeed.sol"; 
 
-
+interface IWeth is IERC20 {
+    function approve(address, uint) external;
+}
 //This test must be run as a mainnet fork, to work correctly
 contract HelperTest is FrontierV2Test {
 
     BalancerHelper helper;
-    BorrowContract borrowContract;
     bytes32 borrowHash;
     address userPk;
     uint maxBorrowAmount;
@@ -35,7 +36,6 @@ contract HelperTest is FrontierV2Test {
         vm.stopPrank();
         
         dbr = DolaBorrowingRights(0xAD038Eb671c44b853887A7E32528FaB35dC5D710);
-        borrowContract = new BorrowContract(address(market), payable(address(WETH)));
         address vault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
         bytes32 poolId = 0x445494f823f3483ee62d854ebc9f58d5b9972a25000200000000000000000415;
         helper = new BalancerHelper(address(DOLA), address(dbr), address(WETH), poolId, vault); 
@@ -88,6 +88,34 @@ contract HelperTest is FrontierV2Test {
         assertEq(DOLA.balanceOf(userPk), maxBorrowAmount / 2, "failed to borrow DOLA");
     }
 
+    function testDepositAndBorrowOnBehalf2() public {
+        uint duration = 365 days;
+
+        wethTestAmount = 1 ether / 100;
+        maxBorrowAmount = 106 ether / 100;
+        market = Market(0x63Df5e23Db45a2066508318f172bA45B9CD37035);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(1, getBorrowHash(maxBorrowAmount, 0));
+        IWeth weth = IWeth(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+        vm.prank(0xF04a5cC80B1E94C69B48f5ee68a08CD2F09A7c3E);
+        weth.transfer(userPk, 1 ether);
+        borrowController = BorrowController(0x20C7349f6D6A746a25e66f7c235E96DAC880bc0D);
+        vm.prank(gov);
+        borrowController.allow(address(helper));
+        vm.startPrank(userPk, userPk);
+        weth.approve(address(helper), type(uint).max);
+        (uint dolaNeeded, uint dbrNeeded) = helper.approximateDolaAndDbrNeeded(1 ether, duration, 8);
+        emit log_named_uint("Dola needed", dolaNeeded);
+        emit log_named_uint("Dbr needed", dbrNeeded);
+        helper.depositAndBorrowOnBehalf(IMarket(address(market)), wethTestAmount, 1 ether, maxBorrowAmount, duration, block.timestamp, v, r, s);
+        vm.stopPrank();
+
+        assertEq(WETH.balanceOf(address(market.escrows(userPk))), wethTestAmount, "failed to deposit WETH");
+        assertEq(WETH.balanceOf(userPk), 0, "failed to deposit WETH");
+        
+        assertGt(duration, market.debts(userPk) * 365 days / dbr.balanceOf(userPk) - 1 days); 
+        assertEq(DOLA.balanceOf(userPk), 1 ether, "failed to borrow DOLA");
+    }
+
     function testDepositNativeEthAndBorrowOnBehalf() public {
         uint duration = 365 days;
 
@@ -114,6 +142,7 @@ contract HelperTest is FrontierV2Test {
         deposit(wethTestAmount);
         vm.stopPrank();
         vm.prank(userPk, userPk);
+
         helper.borrowOnBehalf(IMarket(address(market)), maxBorrowAmount / 2, maxBorrowAmount, duration, block.timestamp, v, r, s);
 
         assertEq(WETH.balanceOf(address(market.escrows(userPk))), wethTestAmount, "failed to deposit WETH");
@@ -257,5 +286,27 @@ contract HelperTest is FrontierV2Test {
                     )
                 );
         return withdrawHash;
+    }
+
+    function getBorrowHash(uint amount, uint nonce) public view returns(bytes32){
+        bytes32 hash = keccak256(
+                    abi.encodePacked(
+                        "\x19\x01",
+                        market.DOMAIN_SEPARATOR(),
+                        keccak256(
+                            abi.encode(
+                                keccak256(
+                                    "BorrowOnBehalf(address caller,address from,uint256 amount,uint256 nonce,uint256 deadline)"
+                                ),
+                                address(helper),
+                                userPk,
+                                amount,
+                                nonce,
+                                block.timestamp
+                            )
+                        )
+                    )
+                );
+        return hash;
     }
 }
