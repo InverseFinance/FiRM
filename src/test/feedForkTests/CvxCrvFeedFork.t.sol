65645843ac64c20454fdde98a99c2e6d2f60566e
08xmt <markus.08xmt@gmail.com>
2023-04-17 12:27:39 +0200

    Add access control tests

--- src/test/feedForkTests/CvxCrvFeedFork.t.sol
+++ src/test/feedForkTests/CvxCrvFeedFork.t.sol
@@ -20,16 +20,17 @@ contract CvxCrvFeedFork is Test {
     ICurvePool curvePool = ICurvePool(0x971add32Ea87f10bD192671630be3BE8A11b8623);
     IERC20 cvxCrv = IERC20(0x62B9c7356A2Dc64a1969e19C23e4f579F9810Aa7);
     address cvxCrvHolder = 0x3Fe65692bfCD0e6CF84cB1E7d24108E434A7587e;
+    ConvexCurvePriceFeed feed;
 
     function setUp() public {
         string memory url = vm.rpcUrl("mainnet");
         vm.createSelectFork(url);
+        feed = new ConvexCurvePriceFeed();
     }
     /**
     function testSwap7500000() public {
         swapXForY(cvxCrvHolder, 100_000 ether, 75);
     }
-    */
     function testEMA() public {
         emit log_named_uint("Price before", curvePool.get_p());
         emit log_named_uint("Swap amount", cvxCrv.balanceOf(cvxCrvHolder));
@@ -39,6 +40,52 @@ contract CvxCrvFeedFork is Test {
             emit log_uint(curvePool.price_oracle());
         }
         emit log_named_uint("Price after", curvePool.get_p());
+    }
+    */
+
+    function testSetMinCrvPerCvxCrvRatio_accessControl() public {
+        vm.prank(feed.gov());
+        uint newRatio = 10**18 - 10**17;
+        feed.setMinCrvPerCvxCrvRatio(newRatio);
+        assertEq(feed.minCrvPerCvxCrvRatio(), newRatio);
+
+        vm.prank(feed.guardian());
+        newRatio = 10**18 - 10**17*2;
+        feed.setMinCrvPerCvxCrvRatio(newRatio);
+        assertEq(feed.minCrvPerCvxCrvRatio(), newRatio);
+
+        vm.prank(address(0xA));
+        vm.expectRevert("ONLY GOV OR GUARDIAN");
+        newRatio = 10**18 - 10**17*3;
+        feed.setMinCrvPerCvxCrvRatio(newRatio);
+    }
+
+    function testSetGuardian_accessControl() public {
+        vm.prank(feed.guardian());
+        vm.expectRevert("ONLY GOV");
+        feed.setGuardian(address(0xA));
+
+        vm.prank(address(0xA));
+        vm.expectRevert("ONLY GOV");
+        feed.setGuardian(address(0xA));
+
+        vm.prank(feed.gov());
+        feed.setGuardian(address(0xA));
+        assertEq(feed.guardian(), address(0xA));
+    }
+
+    function testSetGov_accessControl() public {
+        vm.prank(feed.guardian());
+        vm.expectRevert("ONLY GOV");
+        feed.setGov(address(0xA));
+
+        vm.prank(address(0xA));
+        vm.expectRevert("ONLY GOV");
+        feed.setGov(address(0xA));
+
+        vm.prank(feed.gov());
+        feed.setGov(address(0xA));
+        assertEq(feed.gov(), address(0xA));
     }
 
     function swapXForY(address xHolder, uint amount, uint times) public {
@@ -57,5 +104,6 @@ contract CvxCrvFeedFork is Test {
         curvePool.exchange(1, 0, cvxCrv.balanceOf(xHolder), 1);
         vm.stopPrank();   
     }
+
 }
 
