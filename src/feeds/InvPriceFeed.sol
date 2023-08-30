a4a7e0dccad0f299515cfed2f5345b7bc69ad4cf
lacoop6tu <burzi.andrea@gmail.com>
2023-08-30 08:58:12 -0500

    add USDC/USD fallback, add test

--- src/feeds/InvPriceFeed.sol
+++ src/feeds/InvPriceFeed.sol
@@ -2,7 +2,11 @@
 
 pragma solidity ^0.8.4;
 
+import {console} from "forge-std/console.sol";
+
 interface IChainlinkFeed {
+    function aggregator() external view returns (address aggregator);
+
     function decimals() external view returns (uint8);
 
     function latestRoundData()
@@ -17,23 +21,43 @@ interface IChainlinkFeed {
         );
 }
 
+interface IAggregator {
+    function maxAnswer() external view returns (int192);
+
+    function minAnswer() external view returns (int192);
+}
+
 interface ICurvePool {
     function price_oracle(uint256 k) external view returns (uint256);
 }
 
-contract InvPriceFeed is IChainlinkFeed {
+contract InvPriceFeed {
     ICurvePool public constant tricrypto =
         ICurvePool(0x5426178799ee0a0181A89b4f57eFddfAb49941Ec);
 
     IChainlinkFeed public constant usdcToUsd =
         IChainlinkFeed(0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6);
 
+    IChainlinkFeed public constant ethToUsd =
+        IChainlinkFeed(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+
+    uint256 public constant ethK = 0;
     uint256 public constant invK = 1;
 
+    uint public minUsdcToUsdRatio = 10 ** 18 / 2;
+
+    /**
+     * @notice Retrieves the latest round data for the INV token price feed
+     * @dev This function calculates the INV price in USD by combining the USDC to USD price from a Chainlink oracle and the INV to USDC ratio from the tricrypto pool
+     * @return roundId The round ID of the Chainlink price feed
+     * @return usdcUsdPrice The latest USDC price in USD computed from the INV/USDC and USDC/USD feeds
+     * @return startedAt The timestamp when the latest round of Chainlink price feed started
+     * @return updatedAt The timestamp when the latest round of Chainlink price feed was updated
+     * @return answeredInRound The round ID in which the answer was computed
+     */
     function latestRoundData()
         external
         view
-        override
         returns (uint80, int256, uint256, uint256, uint80)
     {
         (
@@ -44,16 +68,76 @@ contract InvPriceFeed is IChainlinkFeed {
             uint80 answeredInRound
         ) = usdcToUsd.latestRoundData();
 
+        if (isPriceOutOfBounds(usdcUsdPrice, usdcToUsd)) {
+            (
+                roundId,
+                usdcUsdPrice,
+                startedAt,
+                updatedAt,
+                answeredInRound
+            ) = usdcToUsdFallbackOracle();
+        }
         int256 invDollarPrice = int256(tricrypto.price_oracle(invK));
-        
+
         invDollarPrice =
             (invDollarPrice * usdcUsdPrice * int(10 ** 10)) /
             int(10 ** decimals());
-            
+
         return (roundId, invDollarPrice, startedAt, updatedAt, answeredInRound);
     }
 
-    function decimals() public pure override returns (uint8) {
+    /**
+     * @notice Retrieves number of decimals for the INV price feed
+     * @return decimals The number of decimals for the INV price feed
+     */
+    function decimals() public pure returns (uint8) {
         return 18;
+    }
+
+    /**
+     * @notice Checks if a given price is out of the boundaries defined in the Chainlink aggregator.
+     * @param price The price to be checked.
+     * @param feed The Chainlink feed to retrieve the boundary information from.
+     * @return bool Returns `true` if the price is out of bounds, otherwise `false`.
+     */
+    function isPriceOutOfBounds(
+        int price,
+        IChainlinkFeed feed
+    ) public view returns (bool) {
+        IAggregator aggregator = IAggregator(feed.aggregator());
+        int192 max = aggregator.maxAnswer();
+        int192 min = aggregator.minAnswer();
+        return (max <= price || min >= price);
+    }
+
+    /**
+     * @notice Fetches the ETH to USD price and the ETH to USDC to get USDC/USD price, adjusts the decimals to match Chainlink oracles.
+     * @dev The function assumes that the `price_oracle` returns the price with 18 decimals, and it adjusts to 8 decimals for compatibility with Chainlink oracles.
+     * @return roundId The round ID of the Chainlink price feed
+     * @return usdcToUsdPrice The latest USDC price in USD computed from the ETH/USD and ETH/USDC feeds
+     * @return startedAt The timestamp when the latest round of Chainlink price feed started
+     * @return updatedAt The timestamp when the latest round of Chainlink price feed was updated
+     * @return answeredInRound The round ID in which the answer was computed
+     */
+    function usdcToUsdFallbackOracle()
+        public
+        view
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+        int crvEthToUsdc = int(tricrypto.price_oracle(ethK));
+
+        (
+            uint80 roundId,
+            int256 ethToUsdPrice,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        ) = ethToUsd.latestRoundData();
+
+        int256 usdcToUsdPrice = (crvEthToUsdc * 10 ** 8) /
+            ethToUsdPrice /
+            10 ** 10;
+
+        return (roundId, usdcToUsdPrice, startedAt, updatedAt, answeredInRound);
     }
 }
--- src/test/feedForkTests/InvFeedFork.t.sol
+++ src/test/feedForkTests/InvFeedFork.t.sol
@@ -1,11 +1,11 @@
-// SPDX-License-Identifier: UNLICENSED 
-pragma solidity ^0.8.19; 
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
 
-import "forge-std/Test.sol"; 
-import "src/feeds/InvPriceFeed.sol"; 
+import "forge-std/Test.sol";
+import "src/feeds/InvPriceFeed.sol";
+import "forge-std/console.sol";
 
 contract InvFeedFork is Test {
-
     InvPriceFeed feed;
 
     function setUp() public {
@@ -15,20 +15,155 @@ contract InvFeedFork is Test {
     }
 
     function test_decimals() public {
-       assertEq(feed.decimals(), 18);
+        assertEq(feed.decimals(), 18);
     }
 
     function test_latestRoundData() public {
-        (uint80 clRoundId, int256 clInvUsdPrice, uint clStartedAt, uint clUpdatedAt, uint80 clAnsweredInRound) = feed.usdcToUsd().latestRoundData();
-        (uint80 roundId, int256 invUsdPrice, uint startedAt, uint updatedAt, uint80 answeredInRound) = feed.latestRoundData();
+        (
+            uint80 clRoundId,
+            int256 clUsdcToUsdPrice,
+            uint clStartedAt,
+            uint clUpdatedAt,
+            uint80 clAnsweredInRound
+        ) = feed.usdcToUsd().latestRoundData();
+        (
+            uint80 roundId,
+            int256 invUsdPrice,
+            uint startedAt,
+            uint updatedAt,
+            uint80 answeredInRound
+        ) = feed.latestRoundData();
         assertEq(roundId, clRoundId);
         assertEq(startedAt, clStartedAt);
         assertEq(updatedAt, clUpdatedAt);
-        assertEq(answeredInRound, clAnsweredInRound);       
+        assertEq(answeredInRound, clAnsweredInRound);
 
         uint256 invUSDCPrice = feed.tricrypto().price_oracle(1);
-        uint256 estimatedInvUSDPrice = (invUSDCPrice * uint256(clInvUsdPrice) * 10 ** 10) / 10 ** 18;
-        
+        uint256 estimatedInvUSDPrice = (invUSDCPrice *
+            uint256(clUsdcToUsdPrice) *
+            10 ** 10) / 10 ** 18;
+
         assertEq(uint256(invUsdPrice), estimatedInvUSDPrice);
+    }
+
+    function testWillReturnFallbackWhenOutOfMaxBounds() public {
+        (
+            uint80 clRoundId,
+            int256 usdcToUsdPrice,
+            uint clStartedAt,
+            uint clUpdatedAt,
+            uint80 clAnsweredInRound
+        ) = feed.usdcToUsd().latestRoundData();
+        (
+            uint80 clRoundId2,
+            int256 ethToUsdPrice,
+            uint clStartedAt2,
+            uint clUpdatedAt2,
+            uint80 clAnsweredInRound2
+        ) = feed.ethToUsd().latestRoundData();
+        vm.mockCall(
+            address(feed.usdcToUsd()),
+            abi.encodeWithSelector(IChainlinkFeed.latestRoundData.selector),
+            abi.encode(
+                clRoundId,
+                10 ** 12,
+                clStartedAt,
+                clUpdatedAt,
+                clAnsweredInRound
+            )
+        );
+        (
+            uint80 roundId,
+            int256 invUsdPrice,
+            uint startedAt,
+            uint updatedAt,
+            uint80 answeredInRound
+        ) = feed.latestRoundData();
+
+        assertEq(clRoundId2, roundId);
+        assertEq(clStartedAt2, startedAt);
+        assertEq(clUpdatedAt2, updatedAt);
+        assertEq(clAnsweredInRound2, answeredInRound);
+
+        uint256 invUSDCPrice = feed.tricrypto().price_oracle(1);
+        (, int256 usdcFallback, , , ) = feed.usdcToUsdFallbackOracle();
+
+        uint256 estimatedInvUSDPrice = (invUSDCPrice *
+            uint256(usdcFallback) *
+            10 ** 10) / 10 ** 18;
+
+        assertEq(uint256(invUsdPrice), estimatedInvUSDPrice);
+    }
+
+    function testWillReturnFallbackWhenOutOfMinBounds() public {
+        (
+            uint80 clRoundId,
+            int256 usdcToUsdPrice,
+            uint clStartedAt,
+            uint clUpdatedAt,
+            uint80 clAnsweredInRound
+        ) = feed.usdcToUsd().latestRoundData();
+        (
+            uint80 clRoundId2,
+            int256 ethToUsdPrice,
+            uint clStartedAt2,
+            uint clUpdatedAt2,
+            uint80 clAnsweredInRound2
+        ) = feed.ethToUsd().latestRoundData();
+        vm.mockCall(
+            address(feed.usdcToUsd()),
+            abi.encodeWithSelector(IChainlinkFeed.latestRoundData.selector),
+            abi.encode(
+                clRoundId2,
+                10,
+                clStartedAt2,
+                clUpdatedAt2,
+                clAnsweredInRound2
+            )
+        );
+        (
+            uint80 roundId,
+            int256 invUsdPrice,
+            uint startedAt,
+            uint updatedAt,
+            uint80 answeredInRound
+        ) = feed.latestRoundData();
+
+        assertEq(clRoundId2, roundId);
+        assertEq(clStartedAt2, startedAt);
+        assertEq(clUpdatedAt2, updatedAt);
+        assertEq(clAnsweredInRound2, answeredInRound);
+
+        uint256 invUSDCPrice = feed.tricrypto().price_oracle(1);
+        (, int256 usdcFallback, , , ) = feed.usdcToUsdFallbackOracle();
+
+        uint256 estimatedInvUSDPrice = (invUSDCPrice *
+            uint256(usdcFallback) *
+            10 ** 10) / 10 ** 18;
+
+        assertEq(uint256(invUsdPrice), estimatedInvUSDPrice);
+    }
+
+    function test_compare_oracle() public {
+        (
+            ,
+            int256 invUsdPrice,
+            ,
+            ,
+        ) = feed.latestRoundData();
+
+        (, int256 ethToUsdPrice, , , ) = feed.ethToUsd().latestRoundData();
+        vm.mockCall(
+            address(feed.usdcToUsd()),
+            abi.encodeWithSelector(IChainlinkFeed.latestRoundData.selector),
+            abi.encode(0, 10, 0, 0, 0)
+        );
+        (, int256 invUsdPriceFallback, , , ) = feed.latestRoundData();
+
+        assertApproxEqAbs(
+            uint256(invUsdPrice),
+            uint256(invUsdPriceFallback),
+            0.2 ether
+        ); // 0.2 dollar
     }
 }
