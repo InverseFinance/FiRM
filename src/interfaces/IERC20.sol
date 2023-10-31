cf0e0c3ecc1f76f89913aa68651c26dd13c093d1
08xmt <markus.08xmt@gmail.com>
2023-10-31 15:36:28 +0800

    Update IERC20.sol with mintable

--- src/interfaces/IERC20.sol
+++ src/interfaces/IERC20.sol
@@ -8,6 +8,12 @@ interface IERC20 {
     function allowance(address from, address to) external view returns (uint);
 }
 
+interface IMintable is IERC20 {
+    function mint(address,uint) external;
+    function burn(uint) external;
+    function addMinter(address minter) external;
+}
+
 interface IDelegateableERC20 is IERC20 {
     function delegate(address delegatee) external;
     function delegates(address delegator) external view returns (address delegatee);
