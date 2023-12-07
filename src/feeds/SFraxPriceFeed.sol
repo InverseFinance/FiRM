91050135d17ad7c1a4ceb6320488cf3b40232422
lacoop6tu <burzi.andrea@gmail.com>
2023-12-07 10:19:24 -0500

    add VaultHelper and SFraxPriceFeed, add tests

---
+++ src/feeds/SFraxPriceFeed.sol
@@ -0,0 +1,200 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import {IERC4626} from "lib/openzeppelin-contracts/contracts/interfaces/IERC4626.sol";
+
+interface IChainlinkFeed {
+    function aggregator() external view returns (address aggregator);
+
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 price,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+}
+
+interface IAggregator {
+    function maxAnswer() external view returns (int192);
+
+    function minAnswer() external view returns (int192);
+}
+
+
+contract SFraxPriceFeed {
+    error OnlyGov();
+
+    IERC4626 public constant sFrax =
+        IERC4626(0xA663B02CF0a4b149d2aD41910CB81e23e1c41c32);
+
+    IChainlinkFeed public constant fraxToUsd =
+        IChainlinkFeed(0xB9E1E3A9feFf48998E45Fa90847ed4D467E8BcfD);
+    
+    IChainlinkFeed public constant ethToUsd =
+        IChainlinkFeed(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+    
+    IChainlinkFeed public constant fraxToEth =
+        IChainlinkFeed(0x14d04Fff8D21bd62987a5cE9ce543d2F1edF5D3E);
+
+    uint256 public ethHeartbeat = 1 hours;
+    uint256 public fraxToEthHeartbeat = 24 hours;
+
+    address public gov = 0x926dF14a23BE491164dCF93f4c468A50ef659D5B;
+
+    modifier onlyGov() {
+        if(msg.sender != gov) revert OnlyGov();
+        _;
+    }
+
+    /**
+     * @notice Retrieves the latest round data for sFrax price feed
+     * @dev This function calculates the sFrax price in USD by combining the Frax to USD price from a Chainlink oracle 
+     * and the Frax per sFrax ratio from the sFrax vault contract. 
+     * If Frax/USD price is out of bounds, it will fallback to ETH/USD and Frax/ETH oracles to get the price.
+     * @return roundId The round ID of the Chainlink price feed
+     * @return sFraxUsdPrice The latest sFrax price in USD
+     * @return startedAt The timestamp when the latest round of Chainlink price feed started
+     * @return updatedAt The timestamp when the latest round of Chainlink price feed was updated
+     * @return answeredInRound The round ID in which the answer was computed
+     */
+    function latestRoundData()
+        public
+        view
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+             (
+            uint80 roundId,
+            int256 fraxUsdPrice,
+            uint startedAt,
+            uint updatedAt,
+            uint80 answeredInRound
+        ) = fraxToUsd.latestRoundData();
+
+
+        uint256 fraxPerShare = sFrax.convertToAssets(1 ether);
+
+        if (isPriceOutOfBounds(fraxUsdPrice, fraxToUsd)) {
+            (
+                roundId,
+                fraxUsdPrice,
+                startedAt,
+                updatedAt,
+                answeredInRound
+            ) = fraxToUsdFallbackOracle();
+        }
+
+        int256 sFraxUsdPrice = int256(fraxPerShare) * fraxUsdPrice / 10 ** 8;
+
+        return (roundId, sFraxUsdPrice, startedAt, updatedAt, answeredInRound);
+    }
+
+    /** 
+    @notice Retrieves the latest price for the wstETH token
+    @return price The latest price for the wstETH token
+    */
+    function latestAnswer() external view returns (int256) {
+        (, int256 price, , , ) = latestRoundData();
+        return price;
+    }
+
+    /**
+     * @notice Retrieves number of decimals for the wstETH price feed
+     * @return decimals The number of decimals for the wstETH price feed
+     */
+    function decimals() external pure returns (uint8) {
+        return 18;
+    }
+
+    /**
+     * @notice Checks if a given price is out of the boundaries defined in the Chainlink aggregator.
+     * @param price The price to be checked.
+     * @param feed The Chainlink feed to retrieve the boundary information from.
+     * @return bool Returns `true` if the price is out of bounds, otherwise `false`.
+     */
+    function isPriceOutOfBounds(
+        int price,
+        IChainlinkFeed feed
+    ) public view returns (bool) {
+        IAggregator aggregator = IAggregator(feed.aggregator());
+        int192 max = aggregator.maxAnswer();
+        int192 min = aggregator.minAnswer();
+        return (max <= price || min >= price);
+    }
+
+    /**
+     * @notice Fetches the ETH to USD price and the Frax/ETH to get Frax/USD price, adjusts the decimals to match Chainlink oracles.
+     * @dev It will return the roundId, startedAt, updatedAt and answeredInRound from the ETH/USD Chainlink price feed when both oracles are not stale,
+     * in which case updatedAt will be zero
+     * @return roundId The round ID of the ETH/USD Chainlink price feed
+     * @return fraxToUsdPrice The latest Frax price in USD computed from the ETH/USD and Frax/ETH feeds
+     * @return startedAt The timestamp when the latest round of ETH/USD Chainlink price feed started
+     * @return updatedAt The timestamp when the latest round of ETH/USD Chainlink price feed was updated
+     * @return answeredInRound The round ID of the ETH/USD Chainlink price feed in which the answer was computed
+     */
+    function fraxToUsdFallbackOracle()
+        public
+        view
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+        (
+            uint80 roundId,
+            int256 ethToUsdPrice,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        ) = ethToUsd.latestRoundData();
+
+        (
+            ,
+            int256 fraxToEthPrice,
+            ,
+            uint256 updatedFraxEth,
+        ) = fraxToEth.latestRoundData();
+
+        int256 fraxToUsdPrice = ethToUsdPrice * fraxToEthPrice / 10**18;
+
+        if(isPriceOutOfBounds(ethToUsdPrice, ethToUsd) || block.timestamp - updatedAt > ethHeartbeat) {
+            // Will force stale price on borrow controller
+            updatedAt = 0;
+        }
+
+        if(isPriceOutOfBounds(fraxToEthPrice, fraxToEth) || block.timestamp - updatedFraxEth > fraxToEthHeartbeat) {
+            // Will force stale price on borrow controller
+            updatedAt = 0;
+        }
+
+        return (roundId, fraxToUsdPrice, startedAt, updatedAt, answeredInRound);
+    }
+
+    /**
+     * @notice Sets a new ETH heartbeat
+     * @dev Can only be called by the current gov address
+     * @param newHeartbeat The new ETH heartbeat
+     */
+    function setEthHeartbeat(uint256 newHeartbeat) external onlyGov {
+        ethHeartbeat = newHeartbeat;
+    }
+
+    /**
+     * @notice Sets a new stETH/ETH heartbeat
+     * @dev Can only be called by the current gov address
+     * @param newHeartbeat The new stETH/ETH heartbeat
+     */
+    function setFraxEthHeartbeat(uint256 newHeartbeat) external onlyGov {
+        fraxToEthHeartbeat = newHeartbeat;
+    }
+
+    /**
+     * @notice Sets a new gov address
+     * @dev Can only be called by the current gov address
+     * @param newGov The new gov address
+     */
+    function setGov(address newGov) external onlyGov {
+        gov = newGov;
+    }
+}
--- test/marketForkTests/SFraxMarketForkTest.t.sol
+++ test/marketForkTests/SFraxMarketForkTest.t.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.13;
 
 import "./MarketBaseForkTest.sol";
 import {Market} from "src/Market.sol";
-import {VaultEscrow} from "src/escrows/VaultEscrow.sol";
+import {SimpleERC20Escrow} from "src/escrows/SimpleERC20Escrow.sol";
+import {SFraxPriceFeed} from "src/feeds/SFraxPriceFeed.sol";
+import {VaultHelper} from "src/escrows/VaultHelper.sol";
 
 contract SFraxMarketForkTest is MarketBaseForkTest {
-    VaultEscrow escrow;
+    SimpleERC20Escrow escrow;
+    SFraxPriceFeed sFraxFeed;
     IERC20 frax = IERC20(0x853d955aCEf822Db058eb8505911ED77F175b99e);
+    IERC20 sFrax = IERC20(0xA663B02CF0a4b149d2aD41910CB81e23e1c41c32);
+    VaultHelper helper;
     address fraxToUsd = 0xB9E1E3A9feFf48998E45Fa90847ed4D467E8BcfD;
 
     function setUp() public {
@@ -15,15 +20,16 @@ contract SFraxMarketForkTest is MarketBaseForkTest {
         string memory url = vm.rpcUrl("mainnet");
         vm.createSelectFork(url);
         
-        escrow = new VaultEscrow(0xA663B02CF0a4b149d2aD41910CB81e23e1c41c32);
-        
+        escrow = new SimpleERC20Escrow();
+        sFraxFeed = new SFraxPriceFeed();
+
         Market market = new Market(
             gov,
             lender,
             pauseGuardian,
             address(escrow),
             IDolaBorrowingRights(address(dbr)),
-            frax,
+            sFrax,
             IOracle(address(oracle)),
             5000,
             5000,
@@ -31,6 +37,110 @@ contract SFraxMarketForkTest is MarketBaseForkTest {
             true
         );
 
-        _advancedInit(address(market), fraxToUsd, true);
+        _advancedInit(address(market), address(sFraxFeed), true);
+
+        // Setup VaultHelper
+        helper = new VaultHelper(address(sFrax), address(market), address(frax));
+    }
+
+    function test_wrapAndDeposit(uint initAmount) public { 
+        vm.assume(initAmount > 3);
+        vm.assume(initAmount < 10000000000000 ether);
+        deal(address(frax), user, initAmount, true);
+        
+        vm.startPrank(user);
+        frax.approve(address(helper), type(uint).max);
+        helper.wrapAndDeposit(user, initAmount);
+        assertEq(frax.balanceOf(user), 0);
+    }
+
+    function test_withdrawAndUnwrap(uint initAmount) public {
+        vm.assume(initAmount > 3);
+        vm.assume(initAmount < 10000000000000 ether);
+   
+        address userPk = vm.addr(1);
+        deal(address(frax), userPk, initAmount, true);
+        
+        vm.startPrank(userPk);
+        frax.approve(address(helper), type(uint).max);
+        helper.wrapAndDeposit(userPk, initAmount/2);
+        // Amount of SHARES to withdraw
+        uint256 withdrawAmount = market.predictEscrow(userPk).balance();
+        
+        bytes32 hash = keccak256(
+                    abi.encodePacked(
+                        "\x19\x01",
+                        market.DOMAIN_SEPARATOR(),
+                        keccak256(
+                            abi.encode(
+                                keccak256(
+                                    "WithdrawOnBehalf(address caller,address from,uint256 amount,uint256 nonce,uint256 deadline)"
+                                ),
+                                address(helper),
+                                userPk,
+                                withdrawAmount,
+                                0,
+                                block.timestamp
+                            )
+                        )
+                    )
+                );
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(1, hash);
+
+        helper.withdrawAndUnwrap(userPk, withdrawAmount, block.timestamp, v, r, s);
+        
+        assertApproxEqAbs(frax.balanceOf(userPk), initAmount, 2);
+        assertEq(market.predictEscrow(userPk).balance(), 0);
+    }
+
+    function test_Errors() public {
+        deal(address(frax), user, 10 ether, true);
+        
+        vm.startPrank(user);
+        frax.approve(address(helper), type(uint).max);
+        vm.expectRevert(abi.encodeWithSelector(VaultHelper.AddressZero.selector));
+        helper.wrapAndDeposit(address(0), 10 ether);
+
+        vm.mockCall(address(sFrax),abi.encodeWithSelector(IERC20.balanceOf.selector, address(helper)), abi.encode(uint(0)));
+        vm.expectRevert(abi.encodeWithSelector(VaultHelper.InsufficientShares.selector));
+        helper.wrapAndDeposit(user, 10 ether);
+
+        vm.expectRevert(abi.encodeWithSelector(VaultHelper.AddressZero.selector));
+        helper.withdrawAndUnwrap(address(0), 10 ether, block.timestamp, 0, bytes32(0), bytes32(0));
+
+        vm.clearMockedCalls();
+
+        address userPk = vm.addr(1);
+        deal(address(frax), userPk, 10 ether, true);
+        
+        vm.startPrank(userPk);
+        frax.approve(address(helper), type(uint).max);
+        helper.wrapAndDeposit(userPk, 10 ether);
+        // Amount of SHARES to withdraw
+        uint256 withdrawAmount = market.predictEscrow(userPk).balance();
+        
+        bytes32 hash = keccak256(
+                    abi.encodePacked(
+                        "\x19\x01",
+                        market.DOMAIN_SEPARATOR(),
+                        keccak256(
+                            abi.encode(
+                                keccak256(
+                                    "WithdrawOnBehalf(address caller,address from,uint256 amount,uint256 nonce,uint256 deadline)"
+                                ),
+                                address(helper),
+                                userPk,
+                                withdrawAmount,
+                                0,
+                                block.timestamp
+                            )
+                        )
+                    )
+                );
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(1, hash);
+
+        vm.mockCall(address(sFrax),abi.encodeWithSelector(IERC20.balanceOf.selector, address(helper)), abi.encode(uint(0)));
+        vm.expectRevert(abi.encodeWithSelector(VaultHelper.InsufficientShares.selector));
+        helper.withdrawAndUnwrap(userPk, withdrawAmount, block.timestamp, v, r, s);
     }
 }
