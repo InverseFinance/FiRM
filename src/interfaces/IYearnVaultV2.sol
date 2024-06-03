d40660531d1798e913ba98215717ef51a1ff21c2
lacoop6tu <burzi.andrea@gmail.com>
2024-06-03 12:01:11 +0200

    add initial DolaFraxBP escrow

---
+++ src/interfaces/IYearnVaultV2.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.13;
+
+interface IYearnVaultV2 {
+    function deposit(
+        uint256 amount,
+        address recipient
+    ) external returns (uint256);
+
+    function withdraw(
+        uint256 shares,
+        address recipient
+    ) external returns (uint256);
+
+    function totalSupply() external view returns (uint256);
+
+    function pricePerShare() external view returns (uint256);
+
+    function totalAssets() external view returns (uint256);
+
+    function lastReport() external view returns (uint256);
+
+    function lockedProfitDegradation() external view returns (uint256);
+
+    function lockedProfit() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+}
---
+++ src/util/YearnVaultV2Helper.sol
@@ -0,0 +1,62 @@
+//SPDX-License-Identifier: None
+pragma solidity ^0.8.0;
+
+import "src/interfaces/IMarket.sol";
+import "src/interfaces/IERC20.sol";
+import "src/interfaces/IYearnVaultV2.sol";
+
+library YearnVaultV2Helper {
+    error DepositLimitExceeded();
+    error NotEnoughShares();
+
+    uint256 public constant scale = 1e18;
+
+    /// @notice View function to calculate collateral amount from asset amount
+    /// @param assetAmount Amount of asset to transform
+    /// @return collateralAmount Amount of collateral received
+    function assetToCollateral(
+        IYearnVaultV2 vault,
+        uint assetAmount
+    ) public view returns (uint collateralAmount) {
+        uint totalSupply = vault.totalSupply();
+        if (totalSupply > 0)
+            return (assetAmount * totalSupply) / getFreeFunds(vault);
+        return assetAmount;
+    }
+
+    /// @notice View function to calculate asset amount from collateral amount
+    /// @param collateralAmount Amount of collateral to transform
+    /// @return assetAmount Amount of asset received
+    function collateralToAsset(
+        IYearnVaultV2 vault,
+        uint collateralAmount
+    ) public view returns (uint assetAmount) {
+        return (collateralAmount * vault.pricePerShare()) / scale;
+    }
+
+    /// @notice View function for the exchange rate between asset and collateral
+    /// @return ratio Amount of asset per share of collateral
+    function assetToCollateralRatio(
+        IYearnVaultV2 vault
+    ) external view returns (uint ratio) {
+        return vault.pricePerShare();
+    }
+
+    function getFreeFunds(IYearnVaultV2 vault) public view returns (uint256) {
+        return vault.totalAssets() - calculateLockedProfit(vault);
+    }
+
+    function calculateLockedProfit(
+        IYearnVaultV2 vault
+    ) public view returns (uint256) {
+        uint256 lockedFundsRatio = (block.timestamp - vault.lastReport()) *
+            vault.lockedProfitDegradation();
+
+        if (lockedFundsRatio < 10 ** 18) {
+            uint256 lockedProfit = vault.lockedProfit();
+
+            return
+                lockedProfit - ((lockedFundsRatio * lockedProfit) / 10 ** 18);
+        } else return 0;
+    }
+}
