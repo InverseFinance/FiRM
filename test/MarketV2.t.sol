3622f4dcb774981a62b9f663de87aa684dc671c4
08xmt <markus.08xmt@gmail.com>
2024-10-04 10:54:16 +0200

    Test getLiquidationIncentive, getLiquidationFee

--- test/MarketV2.t.sol
+++ test/MarketV2.t.sol
@@ -100,6 +100,81 @@ contract MarketV2Test is Test {
         collateralValue = thresholdDiff * 3 / 4 + marketParams.maxLiquidationFactorThreshold * market.decimals();
         liquidationFactor = market.calcLiquidationFactor(marketParams, collateralValue, market.decimals());
         assertEq(3250, liquidationFactor, "Liquidation factor not 3250% when three quarter way between max and min threshold");
+    }
 
+    function testGetLiquidationIncentiveBps() external {
+        uint collateralFactor = marketParams.collateralFactorBps - 1;
+        uint liquidationIncentive = market.getLiquidationIncentiveBps(collateralFactor);
+        uint maxLiquidationIncentive = marketParams.maxLiquidationIncentiveBps;
+        uint thresholdDiff = marketParams.maxLiquidationIncentiveThresholdBps - marketParams.collateralFactorBps;
+        assertEq(0, liquidationIncentive, "Liquidation Incentive not 0% when CF below liquidation levels");
+
+        collateralFactor = marketParams.collateralFactorBps;
+        liquidationIncentive = market.getLiquidationIncentiveBps(collateralFactor);
+        assertEq(0, liquidationIncentive, "Liquidation Incentive not 0% when CF at liquidation threshold");
+
+        collateralFactor = marketParams.maxLiquidationIncentiveThresholdBps;
+        liquidationIncentive = market.getLiquidationIncentiveBps(collateralFactor);
+        assertEq(maxLiquidationIncentive, liquidationIncentive, "Liquidation Incentive not max when CF at max liquidation threshold");
+
+        collateralFactor = marketParams.maxLiquidationIncentiveThresholdBps + 1;
+        liquidationIncentive = market.getLiquidationIncentiveBps(collateralFactor);
+        assertEq(maxLiquidationIncentive, liquidationIncentive, "Liquidation Incentive not max when CF above max liquidation threshold");
+
+        collateralFactor = marketParams.collateralFactorBps + thresholdDiff / 10;
+        liquidationIncentive = market.getLiquidationIncentiveBps(collateralFactor);
+        assertEq(100, liquidationIncentive, "Liquidation Incentive not 1% when 10% to liquidation threshold");
+
+        collateralFactor = marketParams.collateralFactorBps + thresholdDiff / 4;
+        liquidationIncentive = market.getLiquidationIncentiveBps(collateralFactor);
+        assertEq(250, liquidationIncentive, "Liquidation Incentive not 2.5% when 25% to liquidation threshold");
+
+        collateralFactor = marketParams.collateralFactorBps + thresholdDiff / 2;
+        liquidationIncentive = market.getLiquidationIncentiveBps(collateralFactor);
+        assertEq(500, liquidationIncentive, "Liquidation Incentive not 5% when 50% to liquidation threshold");
+
+        collateralFactor = marketParams.collateralFactorBps + thresholdDiff * 3 / 4;
+        liquidationIncentive = market.getLiquidationIncentiveBps(collateralFactor);
+        assertEq(750, liquidationIncentive, "Liquidation Incentive not 7.5% when 75% to liquidation threshold");
+    }
+
+    function testGetLiquidationFeeBps() external {
+        uint collateralFactor = marketParams.collateralFactorBps - 1;
+        uint thresholdDiff = marketParams.zeroLiquidationFeeThresholdBps - marketParams.collateralFactorBps;
+        uint liquidationFee = market.getLiquidationFeeBps(collateralFactor);
+        uint maxLiquidationFee = marketParams.maxLiquidationFeeBps;
+        assertEq(0, liquidationFee, "Liquidation fee not 0% when CF below liquidation levels");
+        
+        collateralFactor = marketParams.collateralFactorBps;
+        liquidationFee = market.getLiquidationFeeBps(collateralFactor);
+        assertEq(maxLiquidationFee, liquidationFee, "Liquidation fee not 10% when CF at liquidation levels");
+
+        collateralFactor = marketParams.zeroLiquidationFeeThresholdBps;
+        liquidationFee = market.getLiquidationFeeBps(collateralFactor);
+        assertEq(0, liquidationFee, "Liquidation fee not 0% when CF at zero fee threshold");
+
+        collateralFactor = marketParams.zeroLiquidationFeeThresholdBps + 1;
+        liquidationFee = market.getLiquidationFeeBps(collateralFactor);
+        assertEq(0, liquidationFee, "Liquidation fee not 0% when CF above zero fee threshold");
+
+        collateralFactor = marketParams.collateralFactorBps + thresholdDiff/10;
+        liquidationFee = market.getLiquidationFeeBps(collateralFactor);
+        assertEq(900, liquidationFee, "Liquidation fee not 9% when 10% towards zero fee threshold");
+
+        collateralFactor = marketParams.collateralFactorBps + thresholdDiff/4;
+        liquidationFee = market.getLiquidationFeeBps(collateralFactor);
+        assertEq(750, liquidationFee, "Liquidation fee not 7.5% when 25% towards zero fee threshold");
+
+        collateralFactor = marketParams.collateralFactorBps + thresholdDiff/2;
+        liquidationFee = market.getLiquidationFeeBps(collateralFactor);
+        assertEq(500, liquidationFee, "Liquidation fee not 5% when 50% towards zero fee threshold");
+
+        collateralFactor = marketParams.collateralFactorBps + thresholdDiff*3/4;
+        liquidationFee = market.getLiquidationFeeBps(collateralFactor);
+        assertEq(250, liquidationFee, "Liquidation fee not 2.5% when 75% towards zero fee threshold");
+
+        collateralFactor = marketParams.collateralFactorBps + thresholdDiff*99/100;
+        liquidationFee = market.getLiquidationFeeBps(collateralFactor);
+        assertEq(10, liquidationFee, "Liquidation fee not 0.1% when 99% towards zero fee threshold");
     }
 }
