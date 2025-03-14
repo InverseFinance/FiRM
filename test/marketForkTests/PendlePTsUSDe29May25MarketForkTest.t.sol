04577329703e662f869d3b8aa865e2dc707f5760
08xmt <markus.08xmt@gmail.com>
2025-03-14 11:32:48 +0100

    Add generalized PTDiscountedNavFeed and PendlePTsUSDE29May25 mArket test

--- test/marketForkTests/PendlePTsUSDe29May25MarketForkTest.t.sol
+++ test/marketForkTests/PendlePTsUSDe29May25MarketForkTest.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "./MarketBaseForkTest.sol";
-import {USDeNavBeforeMaturityFeed} from "src/feeds/USDeNavBeforeMaturityFeed.sol";
+import {PTDiscountedNAVFeed} from "src/feeds/PTDiscountedNAVFeed.sol";
 import {ChainlinkBasePriceFeed} from "src/feeds/ChainlinkBasePriceFeed.sol";
 import {FeedSwitch} from "src/util/FeedSwitch.sol";
 
@@ -13,19 +13,12 @@ interface PendleSparkLinearDiscountOracleFactory {
 
 contract PendlePTsUSDe29May25MarketForkTest is MarketBaseForkTest {
     address USDeFeed = address(0xa569d910839Ae8865Da8F8e70FfFb0cBA869F961);
-    address sUSDeFeed = address(0xFF3BC18cCBd5999CE63E788A1c250a88626aD099);
-    address sUSDe = address(0x9D39A5DE30e57443BfF2A8307A4256c8797A3497);
     address pendlePT = address(0xb7de5dFCb74d25c2f21841fbd6230355C50d9308); // PT sUSDe 29 May 25
     address pendlePTHolder =
         address(0x8C0824fFccBE9A3CDda4c3d409A0b7447320F364);
 
-    ChainlinkBasePriceFeed sUSDeWrappedFeed;
-    USDeNavBeforeMaturityFeed beforeMaturityFeed;
-    ChainlinkBasePriceFeed afterMaturityFeed;
-    address navFeed;
-    PendleSparkLinearDiscountOracleFactory navFactory = PendleSparkLinearDiscountOracleFactory(0xA9A924A4BB95509F77868E086154C25e934F6171);
+    ChainlinkBasePriceFeed USDeWrappedFeed;
     uint256 baseDiscount = 0.2 ether; // 20%
-    FeedSwitch feedSwitch;
     
     function setUp() public {
         //This will fail if there's no mainnet variable in foundry.toml
@@ -51,34 +44,18 @@ contract PendlePTsUSDe29May25MarketForkTest is MarketBaseForkTest {
     }
 
     function _deployFeed() internal returns (address feed) {
-        sUSDeWrappedFeed = new ChainlinkBasePriceFeed(
-            gov,
-            sUSDeFeed,
-            address(0),
-            24 hours
-        );
-        navFeed = navFactory.createWithPt(pendlePT, baseDiscount); 
-        beforeMaturityFeed = new USDeNavBeforeMaturityFeed(
-            address(sUSDeWrappedFeed),
-            address(sUSDe),
-            address(navFeed)
-        );
-        afterMaturityFeed = new ChainlinkBasePriceFeed(
+        USDeWrappedFeed = new ChainlinkBasePriceFeed(
             gov,
             USDeFeed,
             address(0),
             24 hours
         );
         
-        feedSwitch = new FeedSwitch(
-            address(navFeed),
-            address(beforeMaturityFeed),
-            address(afterMaturityFeed),
-            18 hours,
+        feed = address(new PTDiscountedNAVFeed(
+            address(USDeWrappedFeed),
             pendlePT,
-            pauseGuardian
-        );
-        return address(feedSwitch);
+            0.2 ether
+        ));
     }
 
     // Override the function to use the PendlePTHolder to avoid error revert: stdStorage find(StdStorage): Slot(s) not found
