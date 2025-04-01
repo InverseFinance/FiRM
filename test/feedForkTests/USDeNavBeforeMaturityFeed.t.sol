5c7d7cec874ac6f7d8d6b78442040a5f4b599033
0xtj24 <tabboz23@proton.me>
2025-04-01 18:26:05 -0500

    uncomment assertions

--- test/feedForkTests/USDeNavBeforeMaturityFeed.t.sol
+++ test/feedForkTests/USDeNavBeforeMaturityFeed.t.sol
@@ -113,12 +113,12 @@ contract USDeNavBeforeMaturityFeedTest is Test {
             int256(sUSDe.convertToAssets(1e18));
          int256 discountPrice2 = (USDeUsdPriceEst2 * navDiscountedPrice2) / 1e18;
         assertEq(discountPrice2, USDeUsdPrice2);
-        // // Check if the discount is decreasing
-        // assertGt(discount, discount2);
-        // // Check if the price is increasing
-        // assertLt(navDiscountedPrice, navDiscountedPrice2);
-        // assertLt(USDeUsdPrice, USDeUsdPrice2);
-        // assertLt(discountPrice, discountPrice2);
+        // Check if the discount is decreasing
+        assertGt(discount, discount2);
+        // Check if the price is increasing
+        assertLt(navDiscountedPrice, navDiscountedPrice2);
+        assertLt(USDeUsdPrice, USDeUsdPrice2);
+        assertLt(discountPrice, discountPrice2);
     }
     function test_STALE_sUSDeFeed() public {
         vm.mockCall(
