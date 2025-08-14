df365bff550a78a4fbfd68bba8eef8be7c85a4e6
0xtj24 <tabboz23@proton.me>
2025-08-14 09:02:00 +0200

    add PT USDe 25Sep25

--- test/marketForkTests/CrvUSDDolaConvexMarketForkTest.t.sol
+++ test/marketForkTests/CrvUSDDolaConvexMarketForkTest.t.sol
@@ -68,7 +68,7 @@ contract CrvUSDDolaConvexMarketForkTest is MarketBaseForkTest {
     function setUp() public virtual {
         //This will fail if there's no mainnet variable in foundry.toml
         string memory url = vm.rpcUrl("mainnet");
-        vm.createSelectFork(url);
+        vm.createSelectFork(url, 22582376);
 
         _advancedInit(crvUSDDolaConvexAddr, address(crvUSDDolaFeedAddr), true);
 
--- test/marketForkTests/CrvUSDDolaYearnV2MarketForkTest.t.sol
+++ test/marketForkTests/CrvUSDDolaYearnV2MarketForkTest.t.sol
@@ -59,7 +59,7 @@ contract CrvUSDDolaYearnV2MarketForkTest is MarketBaseForkTest {
     function setUp() public virtual {
         //This will fail if there's no mainnet variable in foundry.toml
         string memory url = vm.rpcUrl("mainnet");
-        vm.createSelectFork(url);
+        vm.createSelectFork(url, 22582376);
 
         escrow = new SimpleERC20Escrow();
 
