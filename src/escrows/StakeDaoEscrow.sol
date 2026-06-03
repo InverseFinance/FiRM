865b5aa2c15aa336209e816565525d4b1c05d85d
08xmt <markus.08xmt@gmail.com>
2026-06-03 03:27:18 +0200

    Add NoCallBack deploy error

--- src/escrows/StakeDaoEscrow.sol
+++ src/escrows/StakeDaoEscrow.sol
@@ -28,6 +28,7 @@ contract StakeDaoEscrow {
     error OnlyBeneficiaryOrAllowlist();
     error WrongCollateral();
     error InvalidReceiver();
+    error NoCallbackInMarket();
 
     IRewardVault public immutable rewardVault;
     IAccountant public immutable accountant;
@@ -75,6 +76,7 @@ contract StakeDaoEscrow {
         if (address(_token) != rewardVault.asset()) revert WrongCollateral();
         market = msg.sender;
         if (address(_token) != IMarket(market).collateral()) revert WrongCollateral();
+        if(!IMarket(market).callOnDepositCallback()) revert NoCallbackInMarket();
         token = _token;
         token.approve(address(rewardVault), type(uint256).max);
         beneficiary = _beneficiary;
